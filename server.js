5a4bad4279a5f95d343726f0cce7da54e12e1071
“Angelie <AYu27@MassMutual.com>
2018-06-05 19:04:03 -0400

    test commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/aboutus', middleware.requireAuthentication, function (req, res) {
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.send("Hello Express")
+  res.send("Hello Express!")
 })
 
 app.listen(PORT, ()=>{
