a4f7d9a38ac52a1ef0482b5ced8d2366dad16887
Yu, Angelie <AYu27@MassMutual.com>
2018-06-04 22:55:38 -0400

    initialize repo

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+const PORT = process.evn.PORT || 5000
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/aboutus', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.send("Hello Express")
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Skedit web server listenin on port ${PORT}`)
+})
