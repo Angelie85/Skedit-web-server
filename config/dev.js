3e6a350f842c93fb3bbafd89df07fc0e0cbd1d92
“Angelie <AYu27@MassMutual.com>
2018-06-10 08:01:21 -0400

    remove file

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-// dev.js don't commit this
-module.exports={
-  googleClientID:'132027287797-mrho1erkovpi9i5kfli64cltb6k25ada.apps.googleusercontent.com',
-  googleClientSecret:'xMQ9Edck1gL12sUpTQ0qwpaU',
-  mongoURI: 'mongodb://angelie:password1@ds153700.mlab.com:53700/skedit-dev',
-  cookieKey: 'adsfpwoiejlaskdfladfpowie'
-}
