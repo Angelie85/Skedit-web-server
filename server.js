de01adf887ea59518e7c608f16b452f4f1640bdf
“Angelie <AYu27@MassMutual.com>
2018-06-10 19:43:30 -0400

    fix build

--- server.js
+++ server.js
@@ -30,15 +30,30 @@ var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 //routes
 require ('./routes/authRoutes')(app);
 
+
+// TODO: test and to be removed
 app.get('/aboutus', middleware.requireAuthentication, function (req, res) {
 	res.send('About Us!');
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+// Serve static files from the React app
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 // app.get('/', function(req, res){
