fd20244b265020f23983990cb6a7276eedb1de28
“Angelie <AYu27@MassMutual.com>
2018-06-10 19:52:20 -0400

    fix

--- server.js
+++ server.js
@@ -47,10 +47,10 @@ if (process.env.NODE_ENV === 'production') {
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // const path = require('path');
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 }
 // Serve static files from the React app
 // app.use(express.static(path.join(__dirname, 'client/build')));
